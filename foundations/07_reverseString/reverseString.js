cb8317fe6acff9700a87e2291e17d0c8891f9118
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-03-20 18:15:40 +0800

    finished exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+  let arr = string.split('');
+  return arr
+    .reverse()
+    .join('');
+};  
 
 // Do not edit below this line
 module.exports = reverseString;
