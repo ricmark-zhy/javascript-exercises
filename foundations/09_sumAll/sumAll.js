20eccd814172b3342e19bbe078eb22161f066525
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-03-21 22:31:56 +0800

    finished exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) return 'ERROR';
+  let arr = [num1, num2];
+  arr.sort( (a, b) => a - b );
+  let start = arr[0], last = arr[1]; result = 0;
+  for (let i = start; i <= last; i++){
+    result+=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
