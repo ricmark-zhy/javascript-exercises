1b3e3e728ba6cd0cb67dab8f50796ea7403c10c2
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-03-21 14:07:49 +0800

    finished exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // for (let i = 1; i < arguments.length; i++) {
+  //   for (let j = 0; j < arr.length; j++) {
+  //     if (arr[j] === arguments[i]) {
+  //       arr.splice(j, 1);
+  //       j--;
+  //     }
+  //   }
+  // }
+  // return arr;
+  return arr.filter( value => !args.includes(value) );
 };
 
 // Do not edit below this line
