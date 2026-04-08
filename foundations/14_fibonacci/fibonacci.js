346797ad7028628c744548e5f70f40da7919e3c7
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-04-08 14:05:07 +0800

    finished exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+  n = parseInt(n);
+  if (n < 0) return 'OOPS';
 
+  let first = 0;
+  let second = 1;
+  for (let i = 0; i < n; i++){
+    let temp = first + second;
+    first = second;
+    second = temp;
+  }
+  return first;
 };
 
 // Do not edit below this line
