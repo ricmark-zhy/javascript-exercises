8d49927bdd9781888b682bedf0eb7c92d23cd1ba
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-04-08 14:40:49 +0800

    finished exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const oldest = arr
+    .sort((a, b) => {
+      const age1 = getAge(a.yearOfBirth, a.yearOfDeath);
+      const age2 = getAge(b.yearOfBirth, b.yearOfDeath);
+      return age1 - age2;
+    });
 
+  return oldest[oldest.length - 1];
 };
+
+const getAge = (birth, death) => {
+  if (!death){
+    death = (new Date()).getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
